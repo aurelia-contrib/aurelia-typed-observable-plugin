4aa2e12e1c70a97821b1feea18ee7da6652c3ecb
bigopon <bigopon.777@gmail.com>
2022-04-10 20:18:02 +1000

    chore(deps): upgrade all dependencies

--- demo/resources/index.ts
+++
@@ -1,9 +0,0 @@
-// tslint:disable:no-implicit-dependencies
-import { FrameworkConfiguration } from "aurelia-framework";
-import { PLATFORM } from "aurelia-pal";
-
-export function configure(config: FrameworkConfiguration): void {
-  config.globalResources([
-    //PLATFORM.moduleName("")
-  ]);
-}
